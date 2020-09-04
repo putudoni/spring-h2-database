7c031aea28b995575d41cb249bc3ad2d3693959f
putuwigadoni <putuwigadoni@yahoo.com>
2020-09-04 20:34:44 +0700

    Create AppCommandLineRunner.java

---
+++ src/main/java/com/pnwd/account/AppCommandLineRunner.java
@@ -0,0 +1,33 @@
+package com.pnwd.account;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ *
+ */
+@Component
+public class AppCommandLineRunner implements CommandLineRunner {
+
+	@Autowired
+	private AccountRepository accountRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		List<Account> listAccount = new ArrayList<>();
+		Account account = new Account();
+		for (int i = 1; i <= 10; i++) {
+			account = new Account();
+			account.setName("Account"+i);
+			account.setEmail("account"+i+"@mail.com");
+			listAccount.add(account);
+		}
+		accountRepository.saveAll(listAccount);
+	}
+
+}
