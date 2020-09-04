0d135200db08d5a86bd7c112c6b809c9a52aefa5
putuwigadoni <putuwigadoni@yahoo.com>
2020-09-04 20:31:19 +0700

    Create Account.java

---
+++ src/main/java/com/pnwd/account/Account.java
@@ -0,0 +1,57 @@
+package com.pnwd.account;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ *
+ */
+@Entity
+@Table(name = "account")
+public class Account {
+
+	@Id
+	@GeneratedValue(generator = "system-uuid")
+	@GenericGenerator(name = "system-uuid", strategy = "uuid")
+	private String id;
+	
+	private String name;
+	
+	private String email;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Account [id=" + id + ", name=" + name + ", email=" + email + "]";
+	}
+	
+	
+}
