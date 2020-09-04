efdd8e6d1d634250d76da2b8a263a1612c3f16d8
putuwigadoni <putuwigadoni@yahoo.com>
2020-09-04 20:34:17 +0700

    Create AccountControllerAdvice.java

---
+++ src/main/java/com/pnwd/account/AccountControllerAdvice.java
@@ -0,0 +1,23 @@
+package com.pnwd.account;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * @author Putu Ngurah Wigadoni
+ *
+ */
+@RestControllerAdvice
+public class AccountControllerAdvice {
+
+	@ResponseBody
+	@ExceptionHandler(AccountNotFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String AccountNotFoundHandler(AccountNotFoundException exception) {
+		return exception.getMessage();
+	}
+	
+}
