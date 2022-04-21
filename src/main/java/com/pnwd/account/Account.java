edd80af60bcabec7898ef4565210ec1a639a71aa
putuwigadoni <putuwigadoni@yahoo.com>
2022-04-21 13:38:46 +0700

    Use @Data annotation in Account Entity

--- src/main/java/com/pnwd/account/Account.java
+++ src/main/java/com/pnwd/account/Account.java
@@ -7,6 +7,11 @@ import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
 
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@ToString
 @Entity
 @Table(name = "account")
 public class Account {
@@ -20,34 +25,4 @@ public class Account {
 	
 	private String email;
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	@Override
-	public String toString() {
-		return "Account [id=" + id + ", name=" + name + ", email=" + email + "]";
-	}
-	
-	
 }
